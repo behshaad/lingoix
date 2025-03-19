24dae315ce6d90686f81097acbdee641cca01007
Behshad <behshadrad04@gmail.com>
2025-03-19 18:48:47 +0330

    Add routers under resource set

--- src/router.js
+++ src/router.js
@@ -1,29 +1,3 @@
-// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import HomePage from "./pages/HomePage";
-// import Navbar from "./components/Nav/Navbar";
-// import Resources from "./pages/Resources";
-// import Dashboard from "./pages/StudentDashboard.jsx";
-// import Login from "./components/Authentication/Login.jsx";
-// import SignUp from "./components/Authentication/SignUp";
-
-// const AppRouter = () => {
-//   return (
-//     <Router>
-//       <Navbar />
-//       <div className="content pt-20">
-//         <Routes>
-//           <Route path="/" element={<HomePage />} />
-//           <Route path="/resources" element={<Resources />} />
-//           <Route path="/dashboard" element={<Dashboard />} />
-//           <Route path="/login" element={<Login />} />
-//           <Route path="/SignUp" element={<SignUp />} />
-//         </Routes>
-//       </div>
-//     </Router>
-//   );
-// };
-
-// export default AppRouter;
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Navbar from "./components/Nav/Navbar";
