bf06a9c2b85fe53902dfaa752d09beb88536682f
Behshad <behshadrad04@gmail.com>
2025-03-25 21:01:57 +0330

    Update LehrePage component with English translations for dashboard headings and enhance HomePage layout by commenting out unused components.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -19,6 +19,7 @@ const HomePage = () => {
       <SmartLearnHero />
       <StepbyStep />
       <FeaturesSection />
+      
       {/* <WordSearch/> */}
 
       {/* <Test /> */}
--- src/pages/LehrePage.jsx
+++ src/pages/LehrePage.jsx
@@ -43,23 +43,25 @@ export default function LehrePage() {
 
   return (
     <div className="p-6 text-gray-900  dark:text-white min-h-screen">
-      <h1 className="text-3xl font-bold mb-6">داشبورد اساتید</h1>
+      <h1 className="text-3xl font-bold mb-6">Instructors’ Dashboard</h1>
       <div className="grid grid-cols-3 gap-6">
         <motion.div className="p-4 text-gray-900  dark:text-white rounded-xl shadow-lg info-card">
-          <h2 className="text-xl font-semibold">👨‍🎓 دانشجویان فعال</h2>
+          <h2 className="text-xl font-semibold">👨‍🎓 Active Students</h2>
           <p className="text-3xl font-bold">{activeStudents}</p>
         </motion.div>
         <motion.div className="p-4  rounded-xl shadow-lg info-card">
-          <h2 className="text-xl font-semibold">📈 میانگین نمرات</h2>
+          <h2 className="text-xl font-semibold">📈 Average Scores</h2>
           <p className="text-3xl font-bold">{averageScore}%</p>
         </motion.div>
         <motion.div className="p-4 rounded-xl shadow-lg info-card">
-          <h2 className="text-xl font-semibold">🎯 درصد تکمیل دوره</h2>
+          <h2 className="text-xl font-semibold">🎯 Course Completion Rate</h2>
           <p className="text-3xl font-bold">{completionPercentage}%</p>
         </motion.div>
       </div>
       <div className="mt-8  p-6 rounded-xl shadow-lg">
-        <h2 className="text-xl font-semibold mb-4">📊 نمودار پیشرفت کلی</h2>
+        <h2 className="text-xl font-semibold mb-4">
+          📊 Overall Progress Chart{" "}
+        </h2>
         <ResponsiveContainer width="100%" height={300}>
           <BarChart data={data}>
             <XAxis dataKey="name" />
