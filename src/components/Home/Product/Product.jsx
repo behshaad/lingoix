6169d477596f11345920492819fdf941734804eb
Behshad <behshadrad04@gmail.com>
2025-03-31 23:27:39 +0330

    Remove redundant class from Product component section for improved styling consistency and cleaner JSX structure.

--- src/components/Home/Product/Product.jsx
+++ src/components/Home/Product/Product.jsx
@@ -1,6 +1,6 @@
 const Product = () => {
     return ( <>
-    <section class="py-12 bg-white sm:py-16 lg:py-20">
+    <section class="py-12  sm:py-16 lg:py-20">
     <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
         <div class="text-center">
             <h2 class="text-3xl font-bold leading-tight text-gray-900 sm:text-4xl xl:text-5xl font-pj">Make every step user-centric</h2>
