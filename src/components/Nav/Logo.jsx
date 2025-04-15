7e8492afca3466e3864543977a47c6de4cec7471
Behshad <behshadrad04@gmail.com>
2025-04-15 16:04:14 +0330

    Update Logo component to use new logo image; replace text with image for improved branding consistency in navigation.

--- src/components/Nav/Logo.jsx
+++ src/components/Nav/Logo.jsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
 
+import CommunitySVG from "../../assets/logo/logo.png";
+
 const Logo = () => {
   return (
     <Link
       to="/"
-      className="text-3xl font-semibold text-gray-900 dark:text-white tracking-wide"
+      className="text-3xl font-semibold "
     >
-      Lingoix
+      {/* Lingoix */}
+      <img src={CommunitySVG} alt="lingoix" className="w-16 h-16 rounded " />
     </Link>
   );
 };
